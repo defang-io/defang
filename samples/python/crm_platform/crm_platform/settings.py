33761a0fae8ca60ceb5fa13e45e6a6f9f49aef6d
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-05-02 14:59:11 -0700

    ready for prod

--- samples/python/crm_platform/crm_platform/settings.py
+++ samples/python/crm_platform/crm_platform/settings.py
@@ -28,7 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://hongcheny-django-crm--8000.prod1.defang.dev']
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.prod1.defang.dev',
+    'http://localhost:8000',
+]
 
 # Application definition
 
@@ -46,10 +49,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'crm_platform.urls'
@@ -125,8 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
